839b6d69a74e6f4780eda0355c6ba3b9e5d7e04d
jamesratzlaff <jamesratzlaff@gmail.com>
2019-09-17 05:44:54 -0400

    downgraded TagProcessorServer to be compatible with Java 8, updtd pom

--- src/main/java/com/jamesratzlaff/yaml/spi/impl/service/TagProcessorService.java
+++ src/main/java/com/jamesratzlaff/yaml/spi/impl/service/TagProcessorService.java
@@ -1,7 +1,7 @@
 package com.jamesratzlaff.yaml.spi.impl.service;
 
 import java.util.ServiceLoader;
-import java.util.ServiceLoader.Provider;
+import java.util.stream.StreamSupport;
 
 import org.yaml.snakeyaml.nodes.Node;
 import org.yaml.snakeyaml.nodes.ScalarNode;
@@ -9,7 +9,6 @@ import org.yaml.snakeyaml.nodes.Tag;
 
 import com.jamesratzlaff.yaml.spi.TagProcessor;
 import com.typesafe.config.ConfigIncludeContext;
-import com.typesafe.config.ConfigIncluder;
 import com.typesafe.config.ConfigOrigin;
 import com.typesafe.config.ConfigValue;
 import com.typesafe.config.ConfigValueFactory;
@@ -28,29 +27,27 @@ public class TagProcessorService {
 	
 	private TagProcessorService() {
 		tagProcessors=ServiceLoader.load(TagProcessor.class);
+		tagProcessors.reload();
 	}
 	
 	public ServiceLoader<TagProcessor> getServiceLoader(){
 		return this.tagProcessors;
 	}
 	
-	private Provider<TagProcessor> getProvider(Tag tag){
+	private TagProcessor getProvider(Tag tag){
 		if(tag==null) {
 			tag=Tag.NULL;
 		}
 		Tag toUse = tag;
-		return getServiceLoader().stream().filter(tProcessor->tProcessor.get().isCompatible(toUse)).findFirst().orElse(null);
+		return StreamSupport.stream(getServiceLoader().spliterator(),false).filter(tProcessor->tProcessor.isCompatible(toUse)).findFirst().orElse(null);
 	}
 	
 	public TagProcessor getTagProcessor(Tag tag) {
 		if(tag==null) {
 			tag=Tag.NULL;
 		}
-		Provider<TagProcessor> provider = getProvider(tag);
-		if(provider!=null) {
-			return provider.get();
-		}
-		return null;
+		TagProcessor provider = getProvider(tag);
+		return provider;
 	}
 	
 	public TagProcessor getTagProcessor(Node n) {
